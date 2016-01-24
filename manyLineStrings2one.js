7645d364a0e2eac66daf7e7de84de3b16dade124
Edward17 <edward17@mail.ua>
2016-01-24 11:49:53 +0100

    Code for debugging removed; all in one file now

--- manyLineStrings2one.js
+++ manyLineStrings2one.js
@@ -16,7 +16,6 @@ console.log('end_point: ' + end_point.toString());
         var ok = false;
 
         for (var i = 1; i < lines.length; i++) {
-console.log(lines[i].geometry.coordinates[0].toString());
             if (lines[i].geometry.coordinates[0].toString() == end_point_string) {
                 lines[0].geometry.coordinates = lines[0].geometry.coordinates.concat(lines[i].geometry.coordinates);
                 lines.splice(i, 1);
@@ -28,7 +27,6 @@ console.log(lines[i].geometry.coordinates[0].toString());
         if (!ok) {
             for (var i = 1; i < lines.length; i++) {
                 var last_point_index = lines[i].geometry.coordinates.length - 1;
-console.log(lines[i].geometry.coordinates[last_point_index].toString());
                 if (lines[i].geometry.coordinates[last_point_index].toString() == end_point_string) {
                     lines[0].geometry.coordinates = lines[0].geometry.coordinates.concat(lines[i].geometry.coordinates.reverse());
                     lines.splice(i, 1);
@@ -40,7 +38,6 @@ console.log(lines[i].geometry.coordinates[last_point_index].toString());
 
         if (!ok) {
             for (var i = 1; i < lines.length; i++) {
-console.log(lines[i].geometry.coordinates[0].toString());
                 if (lines[i].geometry.coordinates[0].toString() == start_point_string) {
                     //lines[0].geometry.coordinates = lines[0].geometry.coordinates.concat(lines[i].geometry.coordinates);
                     lines[0].geometry.coordinates = lines[i].geometry.coordinates.reverse().concat(lines[0].geometry.coordinates);
@@ -54,7 +51,6 @@ console.log(lines[i].geometry.coordinates[0].toString());
         if (!ok) {
             for (var i = 1; i < lines.length; i++) {
                 var last_point_index = lines[i].geometry.coordinates.length - 1;
-console.log(lines[i].geometry.coordinates[last_point_index].toString());
                 if (lines[i].geometry.coordinates[last_point_index].toString() == start_point_string) {
                     //lines[0].geometry.coordinates = lines[0].geometry.coordinates.concat(lines[i].geometry.coordinates.reverse());
                     lines[0].geometry.coordinates = lines[i].geometry.coordinates.concat(lines[0].geometry.coordinates);
@@ -66,12 +62,6 @@ console.log(lines[i].geometry.coordinates[last_point_index].toString());
 
         console.log(lines.length);
     }
-
-    /*for (var i = 0; i < lines.length; i++) {
-        var last_point_index = lines[i].geometry.coordinates.length - 1;
-        document.getElementById('output').innerHTML = document.getElementById('output').innerHTML + 'i = ' + i + ', start_point = ' + lines[i].geometry.coordinates[0].toString() + ', end_point = ' + lines[i].geometry.coordinates[last_point_index].toString() + '<br>';
-    }
-    */
 
     input.features = lines;
     document.getElementById('output').value = JSON.stringify(input, "", 4);
